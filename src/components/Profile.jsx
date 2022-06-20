a403aadebfe5df99e04430e5d3c4b4495193a5a3
avidin91 <a.vidin.91@gmail.com>
2022-06-20 20:15:13 +0300

    git practice

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import s from './Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div className={s.main}>
+                <img src='https://i.pinimg.com/originals/ec/b0/bc/ecb0bc4f76a0bc7e94e4d0e7c2c40e00.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post 1
+                    </div>
+                    <div className={s.item}>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
