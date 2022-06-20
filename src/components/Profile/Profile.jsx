6f172a7f153ada6de5bf12ef75140022a44dbe97
avidin91 <a.vidin.91@gmail.com>
2022-06-20 21:41:00 +0300

    test webstorm

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     return (
         <div className={s.content}>
             <div className={s.main}>
-                <img src='https://i.pinimg.com/originals/ec/b0/bc/ecb0bc4f76a0bc7e94e4d0e7c2c40e00.jpg' />
+                <img src='https://i.pinimg.com/originals/ec/b0/bc/ecb0bc4f76a0bc7e94e4d0e7c2c40e00.jpg' alt='test' />
             </div>
             <div>
                 ava + description
